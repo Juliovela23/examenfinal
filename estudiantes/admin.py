d3b2dc69c7d5c8d7bb1799419939325e39ca8227
Juliovela23 <juliovel86@mesoamericana.edu.gt>
2021-11-29 12:56:20 -0600

    Se añadio el modelo al admin.py

--- estudiantes/admin.py
+++ estudiantes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from estudiantes.models import Estudiante, EstudianteAdmin, Curso,CursoAdmin
 # Register your models here.
+
+admin.site.register(Estudiante, EstudianteAdmin)
+
+admin.site.register(Curso,CursoAdmin)
